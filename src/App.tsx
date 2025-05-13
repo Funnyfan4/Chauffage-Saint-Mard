d119961278dba8f388814b65fedf6ed8cd1ffe35
Funnyfan4 <fanpouriel@gmail.com>
2025-05-13 14:40:38 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  // (Votre code existant inchangé)
+  // ...
+  return (
+    <div className="p-4 max-w-6xl mx-auto">
+      {/* Contenu de votre application */}
+    </div>
+  );
+};
+
+export default App;
